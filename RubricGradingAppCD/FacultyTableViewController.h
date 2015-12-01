53cffbfa678b384c20935b2ad7a6191ee84d5128
Mohamed-Hassan <95:7e:75:c7:f0:4e:09:e4:00:86:35:4a:25:ad:93:cd>
2015-12-01 12:48:24 -0500

    Adding Faculty tableview class

---
+++ RubricGradingAppCD/FacultyTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FacultyTableViewController.h
+//  RubricGradingAppCD
+//
+//  Created by Hassan on 12/1/15.
+//  Copyright © 2015 iosProject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FacultyTableViewController : UITableViewController
+
+@end
