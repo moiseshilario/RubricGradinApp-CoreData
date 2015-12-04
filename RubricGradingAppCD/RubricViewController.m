3fed2e36d32b69f1c07147f2e5e8381794796445
Mohamed-Hassan <95:7e:75:c7:f0:4e:09:e4:00:86:35:4a:25:ad:93:cd>
2015-12-03 22:51:12 -0500

    Fix some bugs

--- RubricGradingAppCD/RubricViewController.m
+++ RubricGradingAppCD/RubricViewController.m
@@ -22,15 +22,75 @@
 @implementation RubricViewController
 
 -(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
-    return 1;
+    if (pickerView == self.pickerOverallProduct) {
+        return 1;
+    }
+    if (pickerView == self.pickerContentAnalysis) {
+        return 1;
+    }
+    if (pickerView == self.pickerDepthAnalysis) {
+        return 1;
+    }
+    if (pickerView == self.pickerDepthContent) {
+        return 1;
+    }
+    if (pickerView == self.pickerQuality) {
+        return 1;
+    }
+    if (pickerView == self.peckerWrintingPro) {
+        return 1;
+    }
+    
+    return 0;
+
 }
 -(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
-    // As we have just one to five grading scale 13
-    return 5;
+    // As we have just one to five grading scale
+    if (pickerView == self.pickerOverallProduct) {
+        return 5;
+    }
+    if (pickerView == self.pickerContentAnalysis) {
+        return 5;
+    }
+    if (pickerView == self.pickerDepthAnalysis) {
+        return 5;
+    }
+    if (pickerView == self.pickerDepthContent) {
+        return 5;
+    }
+    if (pickerView == self.pickerQuality) {
+        return 5;
+    }
+    if (pickerView == self.peckerWrintingPro) {
+        return 5;
+    }
+    return 0;
 }
 -(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+    if (pickerView == self.pickerOverallProduct) {
+        return self.gradingScale[row];
+    }
+    if (pickerView == self.pickerContentAnalysis) {
+        return self.gradingScale[row];
+    }
+    if (pickerView == self.pickerDepthAnalysis) {
+        return self.gradingScale[row];
+    }
+    if (pickerView == self.pickerDepthContent) {
+        return self.gradingScale[row];
+    }
+    if (pickerView == self.pickerQuality) {
+        return self.gradingScale[row];
+    }
+    if (pickerView == self.peckerWrintingPro) {
+        return self.gradingScale[row];
+    }
+    return 0;
+}
+
+-(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{
     
-    return self.gradingScale[row];
+    
 }
 
 - (void)viewDidLoad {
