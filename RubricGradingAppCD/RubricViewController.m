c3c10aaa9343bc91dea930c7fcfbb9ff99e84b8e
Mohamed-Hassan <95:7e:75:c7:f0:4e:09:e4:00:86:35:4a:25:ad:93:cd>
2015-12-03 23:59:37 -0500

    Calculate a giving grade and convert it to percentage

--- RubricGradingAppCD/RubricViewController.m
+++ RubricGradingAppCD/RubricViewController.m
@@ -90,6 +90,162 @@
 
 -(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component{
     
+    double p1;
+    double p2;
+    double p3;
+    double p4;
+    double p5;
+    double p6;
+    double sum;
+    if (pickerView == self.pickerOverallProduct) {
+        
+        // As in the Rubric the firt criteria has wight 0.05 therefore we multiply the point gaving in 0.25
+        
+       switch (row) {
+           case 0:
+               p1 = 5 * 0.25;
+             break;
+           case 1:
+               p1 = 4 * 0.25;
+               break;
+           case 2:
+               p1 = 3 * 0.25;
+               break;
+           case 3:
+               p1 = 2 * 0.25;
+               break;
+           case 4:
+               p1 = 1 * 0.25;
+               break;
+            
+          default:
+              break;
+    }
+    }
+        if (pickerView == self.pickerContentAnalysis) {
+            // this has weigh 0.05
+            
+            switch (row) {
+                case 0:
+                    p2 = 5 * 0.05;
+                    break;
+                case 1:
+                    p2 = 4 * 0.05;
+                    break;
+                case 2:
+                    p2 = 3 * 0.05;
+                    break;
+                case 3:
+                    p2 = 2 * 0.05;
+                    break;
+                case 4:
+                    p2 = 1 * 0.05;
+                    break;
+                    
+                default:
+                    break;
+            }
+    }
+    if (pickerView == self.pickerDepthAnalysis) {
+        // this has weigh 0.20
+        
+        switch (row) {
+            case 0:
+                p3 = 5 * 0.20;
+                break;
+            case 1:
+                p3 = 4 * 0.20;
+                break;
+            case 2:
+                p3 = 3 * 0.20;
+                break;
+            case 3:
+                p3 = 2 * 0.20;
+                break;
+            case 4:
+                p3 = 1 * 0.20;
+                break;
+                
+            default:
+                break;
+        }
+    }
+    if (pickerView == self.pickerDepthContent) {
+        // this has weigh 0.20
+        
+        switch (row) {
+            case 0:
+                p4 = 5 * 0.20;
+                break;
+            case 1:
+                p4 = 4 * 0.20;
+                break;
+            case 2:
+                p4 = 3 * 0.20;
+                break;
+            case 3:
+                p4 = 2 * 0.20;
+                break;
+            case 4:
+                p4 = 1 * 0.20;
+                break;
+                
+            default:
+                break;
+        }
+    }
+    if (pickerView == self.pickerQuality) {
+        // this has weigh 0.15
+        
+        switch (row) {
+            case 0:
+                p5 = 5 * 0.15;
+                break;
+            case 1:
+                p5 = 4 * 0.15;
+                break;
+            case 2:
+                p5 = 3 * 0.15;
+                break;
+            case 3:
+                p5 = 2 * 0.15;
+                break;
+            case 4:
+                p5 = 1 * 0.15;
+                break;
+                
+            default:
+                break;
+        }
+    }
+    if (pickerView == self.peckerWrintingPro) {
+        // this has weigh 0.15
+        
+        switch (row) {
+            case 0:
+                p6 = 5 * 0.15;
+                break;
+            case 1:
+                p6 = 4 * 0.15;
+                break;
+            case 2:
+                p6 = 3 * 0.15;
+                break;
+            case 3:
+                p6 = 2 * 0.15;
+                break;
+            case 4:
+                p6 = 1 * 0.15;
+                break;
+                
+            default:
+                break;
+        }
+    }
+           // Here the sum of the grading
+    sum = p1+p2+p3+p4+p5+p6;
+           // to convert the sumation to percentage
+    sum = (sum / 5) * 100;
     
 }
 
