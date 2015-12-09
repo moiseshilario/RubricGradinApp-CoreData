2e59d51cbe2f1ed2b1e225df0f62b76c39d5e524
Mohamed-Hassan <95:7e:75:c7:f0:4e:09:e4:00:86:35:4a:25:ad:93:cd>
2015-12-08 23:56:45 -0500

    Adding oral rubric UIv[ew and its class

---
+++ RubricGradingAppCD/OralRubricViewController.h
@@ -0,0 +1,14 @@
+//
+//  OralRubricViewController.h
+//  RubricGradingAppCD
+//
+//  Created by Hassan on 12/8/15.
+//  Copyright © 2015 iosProject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OralRubricViewController : UIViewController<UIPickerViewDataSource,UIPickerViewDelegate>
+
+@property NSString *stringOralSum;
+@end
